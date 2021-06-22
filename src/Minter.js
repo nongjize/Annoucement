768840f1b2c3294d8bd1b41fe678f6ab34d5d0d2
nongjize <175292027@qq.com>
2021-06-22 10:30:15 +0800

    first commit

---
+++ src/Minter.js
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import { connectWallet, mintNFT } from "./utils/interact.js";
+
+const Minter = (props) => {
+
+  //State variables
+  const [isConnected, setConnectedStatus] = useState(false);
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+ 
+  useEffect(async () => { //TODO: implement
+    if (window.ethereum) { //if Metamask installed
+      try {
+        const accounts = await window.ethereum.request({ method: "eth_accounts" }) //get Metamask wallet
+        if (accounts.length) { //if a Metamask account is connected
+          setConnectedStatus(true);
+          setWallet(accounts[0]);
+        } else {
+          setConnectedStatus(false);
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      } catch {
+        setConnectedStatus(false);
+        setStatus(
+          "🦊 Connect to Metamask using the top right button. " +
+            walletAddress
+        );
+      }
+    } 
+  });
+
+  const connectWalletPressed = async () => { //TODO: implement
+    const walletResponse = await connectWallet();
+    setConnectedStatus(walletResponse.connectedStatus);
+    setStatus(walletResponse.status);
+    if (isConnected) {
+      setWallet(walletAddress);
+    }
+  };
+
+  const onMintPressed = async () => {
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);
+};
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {isConnected ? (
+          "👛 Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>👛 Connect Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+      <h1 id="title">🧙‍♂️ Alchemy NFT Minter</h1>
+      <p>
+        Simply add your asset's link, name, and description, then press "Mint."
+      </p>
+      <form>
+        <h2>🖼 Link to asset: </h2>
+        <input
+          type="text"
+          placeholder="e.g. https://gateway.pinata.cloud/ipfs/<hash>"
+          onChange={(event) => setURL(event.target.value)}
+        />
+        <h2>🤔 Name: </h2>
+        <input
+          type="text"
+          placeholder="e.g. My first NFT!"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <h2>✍️ Description: </h2>
+        <input
+          type="text"
+          placeholder="e.g. Even cooler than cryptokitties ;)"
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+        Mint NFT
+      </button>
+      <p id="status">
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Minter;
