fd6921ba98a262e15505c3fb9c711cc7c813123a
njz <175292027@qq.com>
2021-06-23 00:13:39 +0800

    late at night

--- src/Minter.js
+++ src/Minter.js
@@ -10,6 +10,9 @@ const Minter = (props) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [url, setURL] = useState("");
+
+  const [NFT_ID_FOR_search, set_NFT_ID_FOR_search] = useState("");
+  const [SearchResult,setSearchResult]=useState("");
  
   useEffect(async () => { //TODO: implement
     if (window.ethereum) { //if Metamask installed
@@ -44,10 +47,17 @@ const Minter = (props) => {
   const onMintPressed = async () => {
     const { status } = await mintNFT(url, name, description);
     setStatus(status);
-};
+  };
+
+  const onNFT_search_Pressed = async () => {
+    const { SearchResult_ } = await mintNFT(NFT_ID_FOR_search);
+    setSearchResult(SearchResult_);
+  };
 
   return (
     <div className="Minter">
+
+      <div>
       <button id="walletButton" onClick={connectWalletPressed}>
         {isConnected ? (
           "👛 Connected: " +
@@ -90,6 +100,52 @@ const Minter = (props) => {
       <p id="status">
         {status}
       </p>
+      </div>
+      
+      <div>
+      <br></br>
+      <h1 id="title">查询NFT</h1>
+      <p>
+        输入id查询NFT 所有人、是否出售（价格）、资产IPFS链接（数字指纹）
+      </p>
+      <form>
+        <h2>NFT ID: </h2>
+        <input
+          type="text"
+          placeholder="34"
+          onChange={(event) => set_NFT_ID_FOR_search(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onNFT_search_Pressed}>
+        查询NFT
+      </button>
+      <p id="status">
+        {SearchResult}
+      </p>
+      </div>
+
+       <div>
+<br></br>
+      <h1 id="title">购买NFT</h1>
+      <p>
+        【输入想要购买的NFT ID进行购买】
+      </p>
+      <form>
+        <h2>NFT ID: </h2>
+        <input
+          type="text"
+          placeholder="34"
+          onChange={(event) => setName(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+        购买
+      </button>
+      <p id="status">
+        {status}
+      </p>
+</div>
+
     </div>
   );
 };
